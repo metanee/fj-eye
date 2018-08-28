eb2d637a0e9d87e5f62ff6c6ef5beb6860776bd1
MivJi <metanee.sae@gmail.com>
2018-08-28 18:41:13 +0700

    Add Chack Session

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
     console.log(res);
     localStorage.setItem('xAuthToken', res.json().token);
     this.loggedIn = true;
-    // location.reload();
+    location.reload();
         },
     error => {
     console.log(error);
@@ -26,6 +26,14 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.loginService.checkSession().subscribe(
+      res => {
+        this.loggedIn = true;
+      },
+      error => {
+        this.loggedIn = false;
+      }
+    );
+  }
   }
 
-}
